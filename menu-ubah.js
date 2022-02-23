de04c6aad37d75c3655bace78b9abd7077b63f83
CodeLysulmad <100286797+CodeLysulmad@users.noreply.github.com>
2022-02-23 19:20:23 +0700

    Add files via upload

---
+++ menu-ubah.js
@@ -0,0 +1,24 @@
+function fungsiHtml() {
+  var x = document.getElementById("html-code");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function fungsiCss() {
+  var c = document.getElementById("css-code");
+  if (c.style.display === "none") {
+    c.style.display = "block";
+  } else {
+    c.style.display = "none";
+  }
+}
+function fungsiJs() {
+  var j = document.getElementById("js-code");
+  if (j.style.display === "none") {
+    j.style.display = "block"
+  } else {
+    j.style.display = "none";
+  }
+}
